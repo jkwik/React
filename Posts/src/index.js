f3d808d670e2e0a2a7866679f97bdfde2481a6f4
JustinKwik <kwik@wisc.edu>
2018-08-18 09:46:10 +0800

    Switches, Routes, Links

--- Posts/src/index.js
+++ Posts/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PostsIndex from "./containers/post_index";
+import promise from "redux-promise";
+import PostsNew from "./containers/posts_new";
 
-import App from './components/app';
-import reducers from './reducers';
+import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
